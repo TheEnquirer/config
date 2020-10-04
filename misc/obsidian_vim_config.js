d2bb23a8c2c4565b6d79711f9e3407ce414720c5
TheEnquirer <huxmarv@nuevaschool.org>
2020-10-04 15:31:46 -0700

    added obsidian vim config... oh boy

---
+++ misc/obsidian_vim_config.js
@@ -0,0 +1,14 @@
+// #########################################################################
+// Past this intil the console every time until an offcial api realease!
+// #########################################################################
+
+CodeMirror.Vim.map('jf', '<Esc>', 'insert')
+CodeMirror.Vim.map('j', 'gj', 'normal')
+CodeMirror.Vim.map('k', 'gk', 'normal')
+
+
+// No I don't have a problem.
+
+
+
+// ok i might have a problem....
